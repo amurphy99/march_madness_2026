1ef3f82fb4fd1727ffc3f99352fbea2ab7bfa50a
amurphy99 <amurphy62299@gmail.com>
2026-03-11 13:36:21 -0500

    more col names

--- src/config.py
+++ src/config.py
@@ -5,12 +5,23 @@ Configuration parameters / constants for the project.
 
 """
 
-# General
-TEAM_BOX_SCORE_COLUMNS = ["2FGM", "2FGA", "3FGM", "3FGA", "FTM", "FTA", "OR", "DR", "Ast", "TO", "Stl", "Blk", "PF"]
+# --------------------------------------------------------------------------------
+# Data Columns
+# --------------------------------------------------------------------------------
+TEAM_BOX_SCORE_COLUMNS = ["Score","2FGM", "2FGA", "3FGM", "3FGA", "FTM", "FTA", "OR", "DR", "Ast", "TO", "Stl", "Blk", "PF"]
+
+W_TEAM_STAT_COLS = [f"W{stat}" for stat in TEAM_BOX_SCORE_COLUMNS]
+L_TEAM_STAT_COLS = [f"W{stat}" for stat in TEAM_BOX_SCORE_COLUMNS]
+
+BOX_SCORE_COLS = W_TEAM_STAT_COLS + L_TEAM_STAT_COLS
 
 
+# --------------------------------------------------------------------------------
 # Default Config
+# --------------------------------------------------------------------------------
+# Number of past tournament years to evaluate with (still uses all regular season data)
 DEFAULT_PAST_YEARS = 7
 
-
+# Number of previous games to store in each team's history
+DEFAULT_HISTORY_LEN = 10
 
