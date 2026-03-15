a914cf9696e16dd86e94415f0b8ed766114f8d21
amurphy99 <amurphy62299@gmail.com>
2026-03-15 13:55:54 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,11 @@ Configuration parameters / constants for the project.
 --------------------------------------------------------------------------------
 `src.config`
 
+Putting some values into their own config classes to help group things up.
+
+TODO: Edit VSCode settings to make it so that some of this stuff is still blue, 
+      even though it is inside a class. It just helps so much with readability...
+
 """
 
 # --------------------------------------------------------------------------------
@@ -21,7 +26,7 @@ BOX_SCORE_DIM  = len(BOX_SCORE_COLS)
 HIST_NUMERIC_DIM = len(BOX_SCORE_COLS) + 3
 
 # --------------------------------------------------------------------------------
-# Default Config
+# Default Pre-processing Config
 # --------------------------------------------------------------------------------
 # Number of past tournament years to evaluate with (still uses all regular season data)
 DEFAULT_PAST_YEARS = 10
@@ -29,8 +34,23 @@ DEFAULT_PAST_YEARS = 10
 # Number of previous games to store in each team's history
 DEFAULT_HISTORY_LEN = 10
 
-# --------------------------------------------------------------------------------
-# Default Config
-# --------------------------------------------------------------------------------
 # Maximum possible Elo change from a single game
 ELO_K = 50.0
+
+# --------------------------------------------------------------------------------
+# Default Training Config
+# --------------------------------------------------------------------------------
+class DEFAULT_TRAINING_CONFIG:
+    DEVICE        = "mps" # mps | cpu
+    BATCH_SIZE    = 128   # Was faster than 64 last time I tested
+    EPOCHS        =  30   # Default epcohs (also get's used by CosineAnnealingLR)
+
+    LEARNING_RATE = 1e-4  # Initial learning rate 
+    ETA_MIN       = 1e-5  # For learning rate schedulers
+    WEIGHT_DECAY  = 1e-6  # Regulization for some optimizers
+
+    # Default Setup
+    OPTIMIZER_TYPE = "adam"
+    SCHEDULER_TYPE = "none"
+
+
